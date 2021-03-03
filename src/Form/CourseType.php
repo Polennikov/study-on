75da122f123056ae17dc2c50dd0b2616992ffd49
Polennikov <art.polennikov@mail.ru>
2021-03-03 04:08:27 +0300

    Release v2

--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -3,21 +3,43 @@
 namespace App\Form;
 
 use App\Entity\Course;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
 
 class CourseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name_course')
-            ->add('description', TextareaType::class)
-            ->add('code')
+            ->add('name_course', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'max' => 255,
+                    ]),
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'constraints' => [
+                    new Length([
+                        'max' => 10000,
+                    ]),
+                ],
+            ])
+            ->add('code', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'max' => 255,
+                    ]),
 
-        ;
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Form/LessonType.php
+++ src/Form/LessonType.php
@@ -2,21 +2,63 @@
 
 namespace App\Form;
 
+use App\Entity\Course;
 use App\Entity\Lesson;
+use App\Repository\CourseRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class LessonType extends AbstractType
 {
+    private $eM;
+
+    public function __construct(EntityManagerInterface $entityManagerInterface)
+    {
+        $this->eM = $entityManagerInterface;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name_lesson')
+            ->add('name_lesson', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'max' => 255,
+                    ]),
+
+                ],
+            ])
             ->add('content_lesson', TextareaType::class)
-            ->add('number_lesson')
-        ;
+            ->add('number_lesson', NumberType::class, [
+                'constraints' => [
+                    new Regex([
+                        'pattern'=>'#^[0-9]+$#',
+                    ]),
+
+                ],
+            ])
+            ->add('course', HiddenType::class);
+        $builder->get('course')
+            ->addModelTransformer(new CallbackTransformer(
+                function (Course $course) {
+                    return $course->getId();
+                },
+                function (int $courseId) {
+
+                    return $this->eM->getRepository(Course::class)->find($courseId);
+                }
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
